a7ed6dbbb8f263e2d6f4593254fc8841f78fe48f
Dominik <dominik_p@wp.pl>
2019-05-29 18:33:19 +0200

    Added coments to RouterCollection.php

--- src/classes/RouterCollection.php
+++ src/classes/RouterCollection.php
@@ -9,30 +9,73 @@ class RouterCollection {
         
     }
 
+    /**
+     * 
+     * Function for set routes with GET request method
+     * 
+     * @param   $uri    string
+     * @param   $name   string
+     * 
+     */
     public static function get($uri, $name)
     {
         $data = self::prepare($uri, $name, 'GET');
         self::setRoute($data);
     }
 
+    /**
+     * 
+     * Function for set routes with POST request method
+     * 
+     * @param   $uri    string
+     * @param   $name   string
+     * 
+     */
     public static function post($uri, $name)
     {
         $data = self::prepare($uri, $name, 'POST');
         self::setRoute($data);
     }
 
+    /**
+     * 
+     * Function for set routes with PUT request method
+     * 
+     * @param   $uri    string
+     * @param   $name   string
+     * 
+     */
     public static function put($uri, $name)
     {
         $data = self::prepare($uri, $name, 'PUT');
         self::setRoute($data);
     }
 
+    /**
+     * 
+     * Function for set routes with DELETE request method
+     * 
+     * @param   $uri    string
+     * @param   $name   string
+     * 
+     */
     public static function delete($uri, $name)
     {
         $data = self::prepare($uri, $name, 'DELETE');
         self::setRoute($data);
     }
 
+    /**
+     * 
+     * Prepare data for insert into $GLOBALS['routes']
+     * 
+     * @param   $uri    string
+     * @param   $name   string
+     * @param   $method string
+     * 
+     * @return  array   array with set uri, controller, action and method
+     * 
+     */
     private static function prepare($uri, $name, $method)
     {
         $url = explode('/', $uri);
@@ -47,6 +90,13 @@ class RouterCollection {
         ];
     }
 
+    /**
+     * 
+     * Set route in $GLOBALS['routes']
+     * 
+     * @param   $data   array
+     * 
+     */
     private static function setRoute($data)
     {
         array_push($GLOBALS['routes'], $data);
