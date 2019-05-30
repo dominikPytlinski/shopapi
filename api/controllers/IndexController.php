a6cd4a9d83203f8c380bcac81001dc37d2968e7e
Dominik <dominik_p@wp.pl>
2019-05-30 19:18:43 +0200

    Created auth function in Model.php

--- api/controllers/IndexController.php
+++ api/controllers/IndexController.php
@@ -14,6 +14,7 @@ class IndexController extends Controller {
 
     public function index($params)
     {
-        Index::find();
+        $token = reset($params);
+        Index::auth($token);
     }
 }
--- src/classes/Model.php
+++ src/classes/Model.php
@@ -6,10 +6,20 @@ use src\classes\Database as DB;
 
 class Model {
 
-    public static function find()
+    public static function auth($token)
     {
-        echo 'find function';
-        DB::connect();
+        $token = filter_var($token, FILTER_SANITIZE_STRING);
+        $sql = 'SELECT id FROM tokens WHERE token = :token';
+        $sth = DB::connect()->prepare($sql);
+        $sth->bindValue(':token', $token, \PDO::PARAM_STR);
+        $sth->execute();
+        $e = $sth->errorInfo();
+        if(empty(end($e))) {
+            $row = $sth->rowCount();
+            return ($row > 0) ? true : false;
+        } else {
+            echo end($e);
+        }
     }
 
 }
